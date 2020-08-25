3f0a39f5cc0353a6cda4e4f562c2eede25cbee32
Ryan Malesic <ryanmalesic@icloud.com>
2020-08-25 17:45:26 -0400

    :tada: initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../scss/styles.scss';
+
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
